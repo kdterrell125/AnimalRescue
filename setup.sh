1df24771af47e4669aba8bdbd8bff1f2a556cbd6
Kandis <kdterrell125@gmail.com>
2021-05-01 15:28:43 -0700

    setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+export DATABASE_URL='postgres://movemtshxspxpr:a0843fe1ef90ada3da45dc278cf4c57051fbc4c264d0b1d8c6c8fc1948dc4b63@ec2-107-22-83-3.compute-1.amazonaws.com:5432/d8864d33bor4mm
+'
+export AUTH0_CLIENT_ID='gwGKpfoZpPLAOiHZs1LUAhxlGbLlHOju'
+export AUTH0_DOMAIN='kdterrell-udacity.us.auth0.com'
+export AUTH0_AUDIENCE='capstone'
+export AUTH0_CALLBACK_URL='http://localhost:5000/'
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
+export ALGORITHMS=['RS256']
+export AUTH0_CLIENT_SECRET='goM_8RuLzc0fVmy6jrH87H-zucGddIeAIj1w3WwgEjwof9vlZ7p1mn0XW30OYjdS'
+
+pip3 install -r requirements.txt
