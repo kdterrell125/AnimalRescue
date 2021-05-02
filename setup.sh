8b6d93166bc68677a11e98744467ea274d4e021f
Kandis <kdterrell125@gmail.com>
2021-05-01 21:14:05 -0700

    deploy

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-export DATABASE_URL='postgres://coqiueowztbhha:32cc37d2d367b1e85588ff1e2675aa3dd18b4d89951c6f8c3986842f8ad26df3@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5ha0safp2m9pe
-'
+export DATABASE_URL='postgres://xfilddcxmfhzue:386e03c127d609958134fe71b79d815aae73ee26bebe804b1af568dc9f40c7eb@ec2-54-163-254-204.compute-1.amazonaws.com:5432/dd0u25khenr1ik'
 export AUTH0_CLIENT_ID='gwGKpfoZpPLAOiHZs1LUAhxlGbLlHOju'
 export AUTH0_DOMAIN='kdterrell-udacity.us.auth0.com'
 export AUTH0_AUDIENCE='capstone'
